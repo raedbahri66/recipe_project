ef54e47c1a44cfe0b929ecfd9dd41a191c56958b
Bahri <raed.bahri@esprit.tn>
2021-10-06 18:13:48 +0200

    Add converters Unit Tests

---
+++ src/test/java/com/bahri/recipe_project/converters/NotesToNotesCommandTest.java
@@ -0,0 +1,49 @@
+package com.bahri.recipe_project.converters;
+
+import com.bahri.recipe_project.commands.NotesCommand;
+import com.bahri.recipe_project.converters.NotesToNotesCommand;
+import com.bahri.recipe_project.domain.Note;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+public class NotesToNotesCommandTest {
+
+    public static final Long ID_VALUE = new Long(1L);
+    public static final String RECIPE_NOTES = "Notes";
+    NotesToNotesCommand converter;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        converter = new NotesToNotesCommand();
+    }
+
+    @Test
+    public void convert() throws Exception {
+        //given
+        Note notes = new Note();
+        notes.setId(ID_VALUE);
+        notes.setRecipeNotes(RECIPE_NOTES);
+
+        //when
+        NotesCommand notesCommand = converter.convert(notes);
+
+        //then
+        assertEquals(ID_VALUE, notesCommand.getId());
+        assertEquals(RECIPE_NOTES, notesCommand.getRecipeNotes());
+    }
+
+    @Test
+    public void testNull() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() throws Exception {
+        assertNotNull(converter.convert(new Note()));
+    }
+}
---
+++ src/test/java/com/bahri/recipe_project/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -0,0 +1,51 @@
+package com.bahri.recipe_project.converters;
+
+
+import com.bahri.recipe_project.commands.UnitOfMeasureCommand;
+import com.bahri.recipe_project.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import com.bahri.recipe_project.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+public class UnitOfMeasureToUnitOfMeasureCommandTest {
+
+    public static final String DESCRIPTION = "description";
+    public static final Long LONG_VALUE = 1L;
+
+    UnitOfMeasureToUnitOfMeasureCommand converter;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        converter = new UnitOfMeasureToUnitOfMeasureCommand();
+    }
+
+    @Test
+    public void testNullObjectConvert() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObj() throws Exception {
+        assertNotNull(converter.convert(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void convert() throws Exception {
+        //given
+        UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(LONG_VALUE);
+        uom.setUom(DESCRIPTION);
+        //when
+        UnitOfMeasureCommand uomc = converter.convert(uom);
+
+        //then
+        assertEquals(LONG_VALUE, uomc.getId());
+        assertEquals(DESCRIPTION, uomc.getUom());
+    }
+
+}
